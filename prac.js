ee8630ade7c8c7167b2f66e7801c922f70843243
chandantk144 <chandantk144@gmail.com>
2023-03-21 12:40:58 +0530

    Chnages

---
+++ prac.js
@@ -0,0 +1,6 @@
+const nam =[
+    "chand",
+    20
+]
+const[name,age]=na;
+console.log(name)
---
+++ validation.js
@@ -0,0 +1,5 @@
+function addition(aa,bb){
+    return aa+bb;
+}
+let sum = addition(10,20);
+console.log(sum)
